54882fe218e085f913683edda37d81d1c1c5ae82
Sergio Ojeda <sergio.yojeda@gmail.com>
2021-01-04 19:28:28 -0500

    mi primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,164 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { futimes } from "fs";
+import { stringify } from "querystring";
+import React, { FormEvent, Fragment, useState, useRef } from "react";
+import { formatDiagnostic } from "typescript";
+import logo from "./logo.svg";
 
-function App() {
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+interface IOrden {
+  mesa: number;
+  personas: number;
+  comida: string;
+  bebida: string;
+  done: boolean;
+}
+
+function App(): JSX.Element {
+  const [orden, setOrden] = useState<IOrden>({
+    mesa: 0,
+    personas: 0,
+    comida: "",
+    bebida: "",
+    done: false,
+  });
+  const [ordenes, setOrdenes] = useState<IOrden[]>([]);
+  const taskInput = useRef<HTMLInputElement>(null);
+
+  function actualizarOrden(e: FormElement) {
+    e.preventDefault();
+    guardarOrden(orden);
+    setOrden(orden)
+    console.log(ordenes);
+  }
+
+  function crearOrden(e: any) {
+    const value = e.target.value;
+    setOrden({
+      ...orden,
+      [e.target.name]: value,
+      done: false,
+    });
+  }
+
+  const guardarOrden = (orden: IOrden) => {
+    const nuevaOrden: IOrden[] = [...ordenes, orden];
+    setOrdenes(nuevaOrden);
+  };
+
+  const actualizarDone = (i: number) => {
+    const nuevaOrden: IOrden[] = [...ordenes];
+    nuevaOrden[i].done = !nuevaOrden[i].done;
+    setOrdenes(nuevaOrden);
+  };
+
+  const eliminarOrden = (i: number) => {
+    const orden: IOrden[] = [...ordenes];
+    orden.splice(i, 1);
+    setOrdenes(orden);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <ul className="nav justify-content-end">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Active
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+      </ul>
+      <div className="row">
+        <div className="col-3">
+          <div className="card mt-5">
+            <div className="card-body">
+              <form onSubmit={actualizarOrden}>
+                <label>
+                  Mesa:
+                  <input
+                    className="form-control mt-2"
+                    name="mesa"
+                    type="number"
+                    onChange={crearOrden}
+                    value={orden.mesa}
+                    ref={taskInput}
+                    autoFocus
+                  />
+                </label>
+                <label>
+                  Personas:
+                  <input
+                    className="form-control mt-2"
+                    name="personas"
+                    type="number"
+                    onChange={crearOrden}
+                    value={orden.personas}
+                  />
+                </label>
+                <label>
+                  Comida:
+                  <input
+                    className="form-control mt-2"
+                    name="comida"
+                    type="text"
+                    onChange={crearOrden}
+                    value={orden.comida}
+                  />
+                </label>
+                <label>
+                  Bebida:
+                  <input
+                    className="form-control mt-2"
+                    name="bebida"
+                    type="text"
+                    onChange={crearOrden}
+                    value={orden.bebida}
+                  />
+                </label>
+                <div className="form-group text-center mt-3">
+                  <button className="btn btn-success ">Crear</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-9">
+          {ordenes.map((o: IOrden, i: number) => (
+            <div className="col-3 card card-body-mt-2" key={i}>
+              <h1 style={{ textDecoration: o.done ? "line-through" : "" }}>
+                Orden {i + ""}
+              </h1>
+              <h2>Mesa: {o.mesa}</h2>
+              <h2>Personas: {o.personas}</h2>
+              <h2>Comida: {o.comida}</h2>
+              <h2>Bebida: {o.bebida}</h2>
+              <button
+                className="btn btn-secondary"
+                onClick={() => actualizarDone(i)}
+              >
+                {o.done ? "✗" : "✓"}
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => eliminarOrden(i)}
+              >
+                🗑
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAv4MEoV-MFFLstYg8qecsQYfB4_G_dBUY",
+    authDomain: "jota-aljafores.firebaseapp.com",
+    projectId: "jota-aljafores",
+    storageBucket: "jota-aljafores.appspot.com",
+    messagingSenderId: "558225820031",
+    appId: "1:558225820031:web:820d00307476a26af8324a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
